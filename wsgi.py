5ae821f6e12e26107bdc32e6ef617e84fcdd5f08
IDIR\lford <leo.ford@gov.bc.ca>
2018-04-25 16:24:50 -0700

    change Hello and add time

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
 from flask import Flask
 application = Flask(__name__)
 
+
+# -- my_now=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
 @application.route("/")
 def hello():
-    return "Hello World! - My Name is Ryan Loiselle, this is the Tuesday Tech Talk about OpenShift and DevOps"
-
+    from datetime import datetime
+    my_message = "Hello World! - My Name is TMR at:" & datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return my_message
 if __name__ == "__main__":
     application.run()
