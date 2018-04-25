41b8ec4af65c36e81e381d0dc82a776bbce3507e
IDIR\lford <leo.ford@gov.bc.ca>
2018-04-25 16:51:45 -0700

    try #2 with date

--- wsgi.py
+++ wsgi.py
@@ -6,9 +6,12 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-#    from datetime import datetime
+    from datetime import datetime
+    my_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 #    my_message = "Hello World! - My Name is TMR at:" & datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    my_message = "Hello World! - My Name is TMR at:" 
 #    return my_message
-    return "Hello World! - My Name is TMR "
+#    return "Hello World! - My Name is TMR "
+    return (my_message, my_now)
 if __name__ == "__main__":
     application.run()
